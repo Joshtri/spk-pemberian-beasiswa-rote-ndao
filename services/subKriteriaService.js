9a89914958f633dc262b4ce6d672c2b395e7f342
Joshtri <stuffofyos1516@gmail.com>
2025-03-28 08:18:44 +0700

    add: implement subKriteria service for managing subKriteria data operations

---
+++ services/subKriteriaService.js
@@ -0,0 +1,30 @@
+import { SubKriteriaRepository } from "@/repositories/subKriteriaRepository";
+
+export const SubKriteriaService = {
+  getAll: () => SubKriteriaRepository.findAll(),
+
+  getById: (id) => SubKriteriaRepository.findById(id),
+
+  create: (payload) => {
+    const { kriteriaId, ...rest } = payload;
+    return SubKriteriaRepository.create({
+      ...rest,
+      kriteria: {
+        connect: { id: kriteriaId },
+      },
+    });
+  },
+
+  update: (id, payload) => {
+    const { kriteriaId, ...rest } = payload;
+    const data = {
+      ...rest,
+      ...(kriteriaId && {
+        kriteria: {
+          connect: { id: kriteriaId },
+        },
+      }),
+    };
+    return SubKriteriaRepository.update(id, data);
+  },
+};
