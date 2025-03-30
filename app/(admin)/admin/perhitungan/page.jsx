313752cebfd48ec3eb718161f28f33723ebe9e04
Joshtri <stuffofyos1516@gmail.com>
2025-03-30 10:35:53 +0700

    feat: create PerhitunganPage component with PerhitunganHero integration

---
+++ app/(admin)/admin/perhitungan/page.jsx
@@ -0,0 +1,8 @@
+import PerhitunganHero from '@/components/Perhitungan/PerhitunganHero'
+import React from 'react'
+
+export default function PerhitunganPage() {
+  return (
+    <PerhitunganHero/>
+  )
+}
