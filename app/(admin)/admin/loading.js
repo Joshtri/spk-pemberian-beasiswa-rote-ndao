5edc89acb6710b458030413d42f6e3291d83586f
Joshtri <stuffofyos1516@gmail.com>
2025-05-26 08:36:46 +0700

    refactor: simplify Loading component by removing unused state and timeout logic

--- app/(admin)/admin/loading.js
+++ app/(admin)/admin/loading.js
@@ -1,20 +1,6 @@
-"use client"
-
 import ThreeLoading from '@/components/three-loading'
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
 export default function Loading() {
-  const [isVisible, setIsVisible] = useState(true);
-
-  useEffect(() => {
-    // Set a timeout of 5 seconds (5000ms) before hiding the loading component
-    const timer = setTimeout(() => {
-      setIsVisible(false);
-    }, 5000);
-
-    // Clean up the timeout when the component unmounts
-    return () => clearTimeout(timer);
-  }, []);
-
-  return isVisible ? <ThreeLoading text={'test'} /> : null;
+  return <ThreeLoading  text={'......'}/>
 }
