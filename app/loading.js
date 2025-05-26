f8ea0a9171abf02356e02c15586794ad7f23e444
Joshtri <stuffofyos1516@gmail.com>
2025-05-26 08:36:40 +0700

    fix: update loading text in Loading component for improved user experience

--- app/loading.js
+++ app/loading.js
@@ -2,5 +2,5 @@ import ThreeLoading from '@/components/three-loading'
 import React from 'react'
 
 export default function Loading() {
-  return <ThreeLoading  text={'test'}/>
+  return <ThreeLoading  text={'......'}/>
 }
